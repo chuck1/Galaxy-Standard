30f281b2e153d11b52f3f28820cb7ab076fb9c2f
Charles Rymal <charlesrymal@gmail.com>
2014-06-10 00:17:40 -0700

    minor

--- src/Galaxy-Standard/registry.cpp
+++ src/Galaxy-Standard/registry.cpp
@@ -7,7 +7,7 @@ gal::std::registry::registry(): next_(0) {
 void                                            gal::std::registry::reg(sp::shared_ptr<gal::std::shared> s) {
 	if(s->i_ == -1) s->i_ = next_++;
 
-	map_.emplace(s->i_, s);
+	map_[s->i_] = s;
 }
 sp::shared_ptr<gal::std::shared>		gal::std::registry::get(gal::std::index_type i) {
 	auto it = map_.find(i);
