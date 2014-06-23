19ac7364a7e643f9d3c1adbd4c3e793c6883dc63
Charles Rymal <charlesrymal@gmail.com>
2014-06-23 00:27:04 -0700

    add timestep

---
+++ include/gal/std/timestep.hpp
@@ -0,0 +1,18 @@
+#ifndef NEBULA_CORE_TIME_STEP_HPP
+#define NEBULA_CORE_TIME_STEP_HPP
+
+
+namespace gal {
+	namespace std {
+		struct timestep {
+			double			time;
+			double			last;
+			double			dt;
+			unsigned int		frame;
+		};
+	}
+}
+
+#endif
+
+
