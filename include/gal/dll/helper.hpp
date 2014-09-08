c8f08c9ebe85dea5f9b070bfa6f5f6e04f29070b
Charles Rymal <charlesrymal@gmail.com>
2014-09-07 22:53:09 -0700

    dll revamp

--- include/gal/dll/helper.hpp
+++ include/gal/dll/helper.hpp
@@ -3,52 +3,90 @@
 
 #include <dlfcn.h>
 
+#include <cassert>
 #include <string>
 
 namespace gal { namespace dll {
 
-	template<class T> class helper {
+	template<class T_, typename... CTOR_ARGS> class helper {
+		public:
+			typedef T_ T;
+		private:
+			helper(helper<T> const & h) {}
 		public:
 			helper(): handle_(0), create_(0), destroy_(0) {}
 
+			helper(helper<T>&& h):
+				handle_(std::move(h.handle_)),
+				create_(std::move(h.create_)),
+				destroy_(std::move(h.destroy_)) {}
 
-			void			open(::std::string filename, ::std::string name_class) {
+
+			void			open(std::string filename, std::string name_class) {
 
 				handle_ = dlopen(filename.c_str(), RTLD_LAZY);
 				if(handle_ == NULL) {
 					perror(dlerror());
-					exit(0);
+					abort();
 				}
 
 				auto name_create = name_class + "_create";
 				auto name_destroy = name_class + "_destroy";
 
 
-				create_	= (T* (*)())dlsym(handle_, name_create.c_str());
+				create_	= (T* (*)(CTOR_ARGS...))dlsym(handle_, name_create.c_str());
 				if(create_ == NULL) {
 					perror(dlerror());
-					exit(0);
+					abort();
 				}
 
 				destroy_ = (void (*)(T*))dlsym(handle_, name_destroy.c_str());
 				if(destroy_ == NULL) {
 					perror(dlerror());
-					exit(0);
+					abort();
 				}
 			}
 			~helper() {
-				dlclose(handle_);
+				if(handle_)
+					dlclose(handle_);
+			}
+			T*		create(CTOR_ARGS... c)
+			{
+				if(!create_) abort();
+				return create_(c...);
+			}
+			void		destroy(T* t)
+			{
+				if(!destroy_) abort();
+				destroy_(t);
 			}
 
-
+		private:
 			void*		handle_;
-
-			T*		(*create_)();
+			T*		(*create_)(CTOR_ARGS...);
 			void		(*destroy_)(T*);
 	};
 
-
-}}}
+	template<typename H> struct deleter
+	{
+		public:
+			typedef typename H::T		T;
+			typedef std::shared_ptr< H >	H_s;
+			
+			deleter(H_s h): _M_helper(h) { assert(h); }
+			
+			deleter(deleter<H>&& d): _M_helper(std::move(d._M_helper)) {}
+			
+			deleter(deleter<H> const & d): _M_helper(d._M_helper) {}
+			
+			void			operator()(T* t) {
+				t->release();
+				_M_helper->destroy(t);
+			}
+		private:
+			H_s		_M_helper;
+	};
+}}
 
 #endif
 
--- test/dll/prog/src/myclass.hpp
+++ test/dll/prog/src/myclass.hpp
@@ -1,10 +1,14 @@
 #ifndef __MYCLASS_H__
 #define __MYCLASS_H__
 
+#include <iostream>
+
 class MyClass {
 	public:
 		MyClass();
 		virtual ~MyClass() {}
+
+		void	release() { std::cout << "release" << std::endl; }
 
 		/* use virtual otherwise linker will try to perform static linkage */
 		virtual void DoSomething();
