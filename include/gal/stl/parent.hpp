c4c18e68955d55323f659e1e2a40053987e46116
chuck <charlesrymal@gmail.com>
2015-03-16 10:52:45 -0700

    remote scene rendered

--- include/gal/stl/parent.hpp
+++ include/gal/stl/parent.hpp
@@ -6,111 +6,113 @@
 #include <gal/stl/wrapper.hpp>
 
 namespace gal { namespace stl {
-		template< typename T, typename S_ = std::shared_ptr<T> >
-		class parent:
-			virtual public gal::itf::shared
+	template< typename T, typename S_ = std::shared_ptr<T> >
+	class parent:
+		virtual public gal::itf::shared,
+		virtual public gal::tmp::Verbosity< gal::stl::parent<T,S_> >
+	{
+	public:
+		friend class boost::serialization::access;
+		using gal::tmp::Verbosity< gal::stl::parent<T,S_> >::printv;
+		//typedef std::shared_ptr<T>		S;
+		typedef S_				S;
+		typedef gal::stl::map<T, S_>		MAP;
+		typedef std::shared_ptr<MAP>		MAP_SHARED;
+		typedef typename MAP::iterator		ITER;
+		typedef typename MAP::FILTER_FUNC	FILTER_FUNC;
+		parent()
 		{
-		public:
-			friend class boost::serialization::access;
-			//typedef std::shared_ptr<T>		S;
-			typedef S_				S;
-			typedef gal::stl::map<T, S_>		MAP;
-			typedef std::shared_ptr<MAP>		MAP_SHARED;
-			typedef typename MAP::iterator		ITER;
-			typedef typename MAP::FILTER_FUNC	FILTER_FUNC;
-			parent()
-			{
-			}
-			void			init(gal::itf::shared * const & parent)
-			{
-				assert_map();
-			}
-			void			insert(S && s)
-			{
-				assert(map_);
-				map_->insert(std::move(s));
-			}
-			S			get(gal::itf::index_type i)
-			{
-				assert(map_);
-				return map_->find(i);
-			}
-			void			erase(gal::itf::index_type i)
-			{
-				auto me = std::dynamic_pointer_cast< gal::stl::parent< T > >(shared_from_this());
-				boost::thread t(boost::bind(
-						&gal::stl::parent< T >::thread_erase,
-						me,
-						i
-						));
-				
-				t.detach();
-			}
-			void			clear()
-			{
-				if(map_) //assert(map_);
-					map_->clear();
-			}
-			S			front(FILTER_FUNC func = FILTER_FUNC())
-			{
-				assert(map_);
-				return map_->front(func);
-			}
-			ITER			begin()
-			{
-				assert(map_);
-				return map_->begin();
-			}
-			ITER			end()
-			{
-				assert(map_);
-				return map_->end();
-			}
-			unsigned int		size()
-			{
-				assert(map_);
-				return map_->size();
-			}
-			bool			empty()
-			{
-				if(map_)
-					return map_->empty();
+		}
+		void			init(gal::itf::shared * const & parent)
+		{
+			assert_map();
+		}
+		void			insert(S && s)
+		{
+			assert(map_);
+			map_->insert(std::move(s));
+		}
+		S			get(gal::itf::index_type i)
+		{
+			assert(map_);
+			return map_->find(i);
+		}
+		void			erase(gal::itf::index_type i)
+		{
+			auto me = std::dynamic_pointer_cast< gal::stl::parent< T > >(shared_from_this());
+			boost::thread t(boost::bind(
+					&gal::stl::parent< T >::thread_erase,
+					me,
+					i
+					));
+			
+			t.detach();
+		}
+		void			clear()
+		{
+			if(map_) //assert(map_);
+				map_->clear();
+		}
+		S			front(FILTER_FUNC func = FILTER_FUNC())
+		{
+			assert(map_);
+			return map_->front(func);
+		}
+		ITER			begin()
+		{
+			assert(map_);
+			return map_->begin();
+		}
+		ITER			end()
+		{
+			assert(map_);
+			return map_->end();
+		}
+		unsigned int		size()
+		{
+			assert(map_);
+			return map_->size();
+		}
+		bool			empty()
+		{
+			if(map_)
+				return map_->empty();
 
-				return true;
+			return true;
+		}
+		void			for_each(std::function<void(S)> const & f)
+		{
+			assert(map_);
+			map_->for_each(f);
+		}
+		void			for_each_int(std::function<int(S)> const & f)
+		{
+			assert(map_);
+			map_->for_each_int(f);
+		}
+	
+	private:
+		void			assert_map()
+		{
+			if(!map_) {
+				map_.reset(new MAP);
+				map_->init(this);
 			}
-			void			for_each(std::function<void(S)> const & f)
-			{
-				assert(map_);
-				map_->for_each(f);
-			}
-			void			for_each_int(std::function<int(S)> const & f)
-			{
-				assert(map_);
-				map_->for_each_int(f);
-			}
-		
-		private:
-			void			assert_map()
-			{
-				if(!map_) {
-					map_.reset(new MAP);
-					map_->init(this);
-				}
-			}
-			template<typename ARCHIVE>
-			void			serialize(ARCHIVE & ar, unsigned int const & version)
-			{
-				assert_map();
-				ar & boost::serialization::make_nvp("map", *map_);
-			}
-			void			thread_erase(gal::itf::index_type i)
-			{
-				map_->erase(i);
-			}
-			MAP_SHARED		map_;
-		};
-	}
-}
+		}
+		template<typename ARCHIVE>
+		void			serialize(ARCHIVE & ar, unsigned int const & version)
+		{
+			printv_func(INFO);
+			assert_map();
+			ar & boost::serialization::make_nvp("map", *map_);
+		}
+		void			thread_erase(gal::itf::index_type i)
+		{
+			map_->erase(i);
+		}
+		MAP_SHARED		map_;
+	};
+}}
 
 #endif
 
